2e28253fec754efd6c0c48d4e27966eb2b02f50e
zacharyhillary <zacharyhillary1@gmail.com>
2023-10-02 01:25:31 -0500

    Update motor_drivers.cpp

--- RTOS_Sliders/motor_drivers.cpp
+++ RTOS_Sliders/motor_drivers.cpp
@@ -3,15 +3,20 @@
 class Motor{
     public:
         Motor(int pinNumber){//overloaded constructor
-            pinNum=pinNumber;
+        
+            pinMode(pinNumber, OUTPUT);// SET PIN TYPE
+            pinNum=pinNumber; /
+            setSpeed(0);// START WITH MOTORS NOT SPINNING!!!!
             }
+
         double speed;
         int pinNum
+
         void setSpeed(double input){
-            if(input>100)speed=100;
-           else if(input<-100)speed=-100;
-           else speed=input;
-            double output = (100 + speed)*1.275;
+            if(input>100)speed=100; // BOUNDING THE INPUT SPEED
+            else if(input<-100)speed=-100;// BOUNDING THE INPUT SPEED
+            else speed=input;
+            double output = (100 + speed)*1.275; // MAKING -100->100 input to a 0->255 output
             analogWrite(pinNum, output);
 
         }
